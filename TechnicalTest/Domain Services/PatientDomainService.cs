5f1341decb88e212432f4aa8358dfa51cb23ff22
Adrian Osuna Fregoso <adrian@onside.co.nz>
2024-05-04 20:09:32 +1200

    Add connection to DB

--- TechnicalTest/Domain Services/PatientDomainService.cs
+++ TechnicalTest/Domain Services/PatientDomainService.cs
@@ -1,9 +1,41 @@
+using Microsoft.Data.SqlClient;
+
 namespace TechnicalTest.Domain_Services;
 
 public class PatientDomainService : IPatientDomainService
 {
     public string GetPatientDetails()
     {
-        return "These are some nice patient details";
+        var result = "";
+        
+        try 
+        { 
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = "",
+                UserID = "",
+                Password = "",
+                InitialCatalog = ""
+            };
+
+            using var connection = new SqlConnection(builder.ConnectionString);
+            connection.Open();       
+
+            const string sql = "SELECT * FROM dbo.Patient";
+
+            using var command = new SqlCommand(sql, connection);
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var test = reader.GetInt32(0);
+                result += $",{test}";
+            }
+        }
+        catch (SqlException e)
+        {
+            result = e.ToString();
+        }
+        
+        return result;
     }
 }
