dc11168958cd40b1083d97bf2695cf05bf2afd53
Adrian Osuna Fregoso <adrian@onside.co.nz>
2024-05-05 13:24:19 +1200

    Tidy up

--- TechnicalTest/Controllers/Home.cs
+++ TechnicalTest/Controllers/Home.cs
@@ -1,16 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using TechnicalTest.ApplicationServices;
 
 namespace TechnicalTest.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class Home(IPatientApplicationService patientApplicationService) : ControllerBase
+public class Home : ControllerBase
 {
     [HttpGet]
     public IActionResult Index()
     {
-        var test = patientApplicationService.Test();
-        return Ok($"Welcome to Medi-Map Technical Test! This comes from the application service layer: {test}");
+        return Ok("Welcome to Medi-Map Technical Test!");
     }
 }
