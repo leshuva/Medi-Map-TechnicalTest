0ea5cc4f90859e7d8ec38ced9974e78ba9249147
Adrian Osuna Fregoso <adrian@onside.co.nz>
2024-05-05 16:59:21 +1200

    Add domain service method to get patient details

--- TechnicalTest/Domain Services/IPatientDomainService.cs
+++ TechnicalTest/Domain Services/IPatientDomainService.cs
@@ -8,4 +8,11 @@ public interface IPatientDomainService
     /// <param name="patientId">The id of the patient to check.</param>
     /// <returns>True if the patient already exists in the database, false otherwise</returns>
     bool IsPatientInDb(int patientId);
+
+    /// <summary>
+    /// Get all details from a patient by id.
+    /// </summary>
+    /// <param name="patientId">The id of the patient</param>
+    /// <returns>A PatientDetails object containing all of the patient details.</returns>
+    PatientDetails GetPatientDetailsById(int patientId);
 }
--- TechnicalTest/Domain Services/PatientDomainService.cs
+++ TechnicalTest/Domain Services/PatientDomainService.cs
@@ -37,14 +37,48 @@ public class PatientDomainService : IPatientDomainService
         return isPatientInDb;
     }
 
+    public PatientDetails GetPatientDetailsById(int patientId)
+    {
+        var patientDetails = new PatientDetails();
+
+        try
+        {
+            var connectionString = GetDbConnectionString();
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            const string sql = "SELECT * FROM dbo.Patient WHERE PatientID = @PatientId";
+
+            using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@PatientId", patientId);
+            using var reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                patientDetails.PatientId = reader.GetInt32(0);
+                patientDetails.FirstName = reader.GetString(1);
+                patientDetails.LastName = reader.GetString(2);
+                patientDetails.Gender = reader.GetString(3);
+                patientDetails.Dob = reader.GetDateTime(4);
+                patientDetails.Height = reader.GetDecimal(5);
+                patientDetails.Weight = reader.GetDecimal(6);
+            }
+        }
+        catch (SqlException)
+        {
+            return new PatientDetails();
+        }
+        
+        return patientDetails;
+    }
+
     private static string GetDbConnectionString()
     {
         var builder = new SqlConnectionStringBuilder
         {
-            DataSource = "cool-server-in-azure.database.windows.net",
-            UserID = "cool-server-in-azure-admin",
-            Password = "hKK@$!2Z9NTT",
-            InitialCatalog = "medimap-code-test"
+            DataSource = "",
+            UserID = "",
+            Password = "",
+            InitialCatalog = ""
         };
 
         return builder.ConnectionString;
